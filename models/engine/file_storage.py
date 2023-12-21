91f682b7bafe032d551f8370dc0b9f17a16d365a
evaniewares <chisopsyelera@gmail.com>
2023-12-21 18:04:10 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,9 +20,11 @@ class FileStorage:
         """
         Delete obj from __objects if it’s inside - if obj is equal to None
         """
-        if obj is not None:
-            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
-            self.all().pop(obj_key, None)
+        if obj is None:
+            return
+        obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+        if obj_key in self.__objects.keys():
+            del self.__objects[obj_key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
