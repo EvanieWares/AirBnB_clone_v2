7f204d7a24395b1b9fb03808394fbbbf3b1f2028
evaniewares <chisopsyelera@gmail.com>
2023-12-21 13:58:14 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,13 +8,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     __tablename__ = 'base_model'
 
-    id = Column(String(60), nullable=False, primary_key=True, default=str(uuid.uuid4()))
+    id = Column(String(60), nullable=False, primary_key=True,
+                default=str(uuid.uuid4()))
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow(), onupdate=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow(),
+                        onupdate=datetime.now())
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
@@ -31,10 +34,10 @@ class BaseModel:
 
             if 'updated_at' in kwargs:
                 self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
             if 'created_at' in kwargs:
                 self.created_at = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
             if '__class__' in kwargs:
                 del kwargs['__class__']
 
