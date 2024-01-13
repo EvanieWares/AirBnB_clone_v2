67c9971cac37d017dddac07ce2a75fd103fd76fe
evaniewares <chisopsyelera@gmail.com>
2024-01-13 10:48:16 +0200

    do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-# Fabfile that generates a .tgz archive from the contents of web_static folder.
+"""
+Fabfile that generates a .tgz archive from the contents of web_static folder.
+"""
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
+    """
+    Creates a tgz archive from the contents of web_static.
+    """
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
