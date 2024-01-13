5ab4cdae3266e65df95a7adf96e69d1090f7230e
evaniewares <chisopsyelera@gmail.com>
2024-01-13 10:47:32 +0200

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,6 +11,7 @@ from fabric.api import local
 
 env.hosts = ["52.91.133.58", "54.157.146.125"]
 
+
 def do_pack():
     """
     Creates a tgz archive from the contents of web_static.
@@ -32,6 +33,7 @@ def do_pack():
         return None
     return file
 
+
 def do_deploy(archive_path):
     """Distributes an archive to web server.
 
@@ -47,26 +49,26 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("sudo rm -rf /data/web_static/releases/{}/".
+    if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("sudo mkdir -p /data/web_static/releases/{}/".
+    if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
-    if run("sudo rm /tmp/{}".format(file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("sudo mv /data/web_static/releases/{}/web_static/* "
+    if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
+    if run("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("sudo rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
