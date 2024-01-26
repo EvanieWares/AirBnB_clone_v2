96bb1b8b7c752df859a3f9d3c342a0964e8e0097
evaniewares <chisopsyelera@gmail.com>
2024-01-26 14:35:42 +0200

    Cities by states - updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
