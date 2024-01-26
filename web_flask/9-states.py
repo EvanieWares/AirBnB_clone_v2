9b03f60bc8f96cfb5402e5cdc2f460f17fd26d02
evaniewares <chisopsyelera@gmail.com>
2024-01-26 17:58:39 +0200

    States and State - updated

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,16 +13,27 @@ def remove_sqlalchemy_session(exception):
     storage.close()
 
 
-@app.route('/states/', defaults={'id': None}, strict_slashes=False)
+@app.route('/states/', defaults={'id': ''}, strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def states(id):
     """
     Route /states/ and /states/<id>
     """
-    states = storage.all("State")
-    if id is not None:
+    states = storage.all("State").values()
+    if id == '':
+        states = sorted(states.values(), key=lambda state: state.name)
+        return render_template('9-states.html', states=states)
+    else:
         id = "State." + id
-    return render_template("9-states.html", states=states, state_id=id)
+        state = states[id]
+        if state:
+            cities = sorted(state.cities, key=lambda city: city.name)
+            return render_template(
+                '9-states.html',
+                state=state,
+                cities = cities
+                )
+    return render_template("9-states.html", not_found=True)
 
 
 if __name__ == '__main__':
