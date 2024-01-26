ec812942f8369545ee19de3312be0372e836febb
evaniewares <chisopsyelera@gmail.com>
2024-01-26 15:46:00 +0200

    Cities by states - fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
 from models import *
+from models import storage
 
 app = Flask(__name__)
 
